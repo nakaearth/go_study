c2b7f0448c73b1fd5c9d0ae8245137ff7c8f8957
nakamura shinichirou <Nakamura.Shinichiro@moneyforward.co.jp>
2015-09-29 13:11:42 +0900

    サンプル追加

---
+++ hello.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+  f "fmt"
+) 
+
+var (
+a string = "test"
+b        = "test2"
+c        = "test3"
+)
+
+var i int
+
+func main() {
+  const Hello string = "Bye bye"
+
+  //var message = "hello world"
+  message := "hello world"
+
+  f.Println(Hello)
+  f.Println(message)
+  f.Println(a)
+  f.Println(b)
+  f.Println(c)
+
+  f.Println(i)
+
+  for  n := 0 ; n < 10 ; n++ {
+    f.Println(n)
+    if n < 4 {
+      f.Println("4よ小さい")
+    } else if n >= 4  {
+      f.Println("真ん中")
+    } else {
+      f.Println("その他")
+    }
+  }
+}
