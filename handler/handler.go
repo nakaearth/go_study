568adbbccc1a23234d9c3eb2f9afd5ecf12a5092
nakaearth <naka5313@gmail.com>
2017-10-18 12:52:36 +0900

    変更

--- handler/handler.go
+++ handler/handler.go
@@ -6,9 +6,6 @@ import (
 )
 
 func MainPage() echo.HandlerFunc {
-    // return func(c echo.Context) error {     //c をいじって Request, Responseを色々する
-    //     return c.String(http.StatusOK, "Hello World")
-    // }
     return func(c echo.Context) error {
         username := c.Param("username")    //プレースホルダusernameの値取り出し
         jsonMap := map[string]string{
